c0baf6559cf9816b3c2f91262330a5b5d50d2026
mmssyy <498232736@qq.com>
2019-09-14 18:07:19 +0800

    99乘法表

--- hw01.py
+++ hw01.py
@@ -1,8 +1,5 @@
-
 for i in range(1, 10):
-        for j in range(i, 10):
-            print(f"{i}*{j}={i*j:<2d}", end='  ')
-        print()
-        print("        "*i, end='')
-
-
+    for j in range(i, 10):
+        print(f"{i}*{j}={i*j:<2d}", end='  ')
+    print()
+    print("        "*i, end='')
